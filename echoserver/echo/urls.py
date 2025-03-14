0b72be5f1ea347e9bfe385cee46922d95eb45fd8
Ksenija <ksyushats01@gmail.com>
2025-03-14 16:23:25 +0500

    gew

--- echoserver/echo/urls.py
+++ echoserver/echo/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('book/new/', views.book_new, name='book_new'),
     path('book/<int:pk>/edit/', views.book_edit, name='book_edit'),
     path('book/<int:pk>/delete/', views.book_delete, name='book_delete'),
-    path('register/', views.register, name='register'),
+    path('register/', views.register,  name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
 ]
